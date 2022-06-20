1a21bc0f2e3d678299df1384e6aceadcb68c8ced
dariaddz <dariadz@gmail.com>
2022-06-20 22:00:21 +0300

    pagination hw4

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,10 +2,12 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const result = await Contact.find({ owner: _id }).populate(
-    "owner",
-    "email _id"
-  );
+  const { page = 1, limit } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "email _id");
   res.json({
     status: "success",
     code: 200,
